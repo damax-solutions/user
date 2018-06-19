9217065ec2a623e26f43caf48383d96468b2e1c2
Dmitri Lakachauskis <lakiboy83@gmail.com>
2018-06-19 12:55:30 +0300

    Fix LocaleChoiceTypeTest.

--- tests/Bridge/Symfony/Bundle/Form/Type/LocaleChoiceTypeTest.php
+++ tests/Bridge/Symfony/Bundle/Form/Type/LocaleChoiceTypeTest.php
@@ -11,7 +11,6 @@ use Damax\User\Bridge\Symfony\Bundle\Form\Type\LocaleChoiceType;
 use Damax\User\Domain\Model\Locale;
 use Damax\User\Domain\NameFormatter\NameFormatter;
 use PHPUnit\Framework\MockObject\MockObject;
-use Symfony\Component\Form\ChoiceList\View\ChoiceView;
 use Symfony\Component\Form\PreloadedExtension;
 use Symfony\Component\Form\Test\TypeTestCase;
 
@@ -58,17 +57,7 @@ class LocaleChoiceTypeTest extends TypeTestCase
         $this->assertFalse($conf->getOption('choice_translation_domain'));
         $this->assertEquals('label.choose_locale', $conf->getOption('placeholder'));
         $this->assertEquals('damax-user', $conf->getOption('translation_domain'));
-        $this->assertSame($this->choices, $conf->getOption('choices'));
-
-        /** @var ChoiceView[] $view */
-        $view = $form->createView()->vars['choices'];
-
-        $this->assertContainsOnlyInstancesOf(ChoiceView::class, $view);
-        $this->assertCount(2, $view);
-        $this->assertEquals('English', $view[0]->label);
-        $this->assertEquals('en', $view[0]->value);
-        $this->assertEquals('Russian', $view[1]->label);
-        $this->assertEquals('ru', $view[1]->value);
+        $this->assertEquals(['English' => 'en', 'Russian' => 'ru'], $conf->getOption('choices'));
     }
 
     /**
@@ -81,7 +70,7 @@ class LocaleChoiceTypeTest extends TypeTestCase
 
         $this->assertTrue($form->isSynchronized());
         $this->assertTrue($form->isValid());
-        $this->assertSame($this->choices[1], $form->getData());
+        $this->assertSame('ru', $form->getData());
     }
 
     /**
